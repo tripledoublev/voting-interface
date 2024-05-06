e2ddc1c29fef7776d228e26bb31a262e212b1792
vincent <vincentcharlebois@gmail.com>
2024-05-06 14:13:37 -0400

    Create Results component

---
+++ src/lib/Results.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let id;
+    export let voteCounts = {};
+</script>
+<div class="m-4">
+    <h1>{@html id}</h1>
+    <h2>Results</h2>
+    <div class="flex flex-col sm:flex-row ml-5">
+        {#each Object.entries(voteCounts) as [response, count]}
+            <div class="results flex flex-col">
+                <span class="txt">{count}</span> {response}
+            </div>
+        {/each}
+    </div>
+</div>
