17b0243f18eacec9c3b16da10c965dd9c1214ccb
vincent <vincentcharlebois@gmail.com>
2024-11-18 09:36:08 -0500

    Create vote4.js

---
+++ public/configs/vote4.js
@@ -0,0 +1,8 @@
+{
+    "id": "Should the following happening be included on Hypha’s official and publicly available timeline?",
+    "year": "2023",
+    "1": "We welcomed HEK as a client and started sharpening our focus on connecting arts and culture organizations with decentralized tech and tools.",
+    "2": "We stayed local for our yearly retreat, spending a week on Toronto Island to talk governance, plan and strategize for the coming year.",
+    "3": "We solidified our reputation as the only people wearing pants at the crazy-making Web3 conflagration.",
+    "4": "We unlocked another level of retreat planning, with activities including an Ikebana workshop and a team-building Dragon Boating outing."
+}
